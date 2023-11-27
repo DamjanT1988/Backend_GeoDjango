bb4b911fa35de5f852ada8da77f2d7876fc4c8bc
DamjanT1988 <Damjan_t@hotmail.com>
2023-11-27 16:22:11 +0100

    271123-1620-klar (ingen debug toolbar)

--- geodjango_app_vm/geodjango_app_vm/settings.py
+++ geodjango_app_vm/geodjango_app_vm/settings.py
@@ -35,18 +35,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "debug_toolbar",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    
+    # "debug_toolbar",    
 ]
 
 MIDDLEWARE = [
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -54,15 +52,14 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-
+    # "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 DEBUG_TOOLBAR_PATCH_SETTINGS = False
 
-
 INTERNAL_IPS = [
-    # ... other internal IPs ...
-    '127.0.0.1',
+    '127.0.0.1:8000',
+    #'192.168.0.1'
 ]
 
 ROOT_URLCONF = "geodjango_app_vm.urls"
--- geodjango_app_vm/geodjango_app_vm/urls.py
+++ geodjango_app_vm/geodjango_app_vm/urls.py
@@ -4,10 +4,13 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    
 ]
 
+'''
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns = [
         path('__debug__/', include(debug_toolbar.urls)),
     ] + urlpatterns
+'''
