00ae678f940f2ed53999ba33638e191454e75c7b
DamjanT1988 <Damjan_t@hotmail.com>
2023-11-28 13:09:38 +0100

    281123-1310-adminsys funkar

--- geodjango_app_vm/auth_app_vm/admin.py
+++ geodjango_app_vm/auth_app_vm/admin.py
@@ -1,4 +1,22 @@
+"""
 from django.contrib import admin
 from .models import User_additional
 
 admin.site.register(User_additional)
+"""
+
+from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+from .models import User_additional
+
+class UserAdditionalInline(admin.StackedInline):
+    model = User_additional
+    can_delete = False
+
+class CustomUserAdmin(UserAdmin):
+    inlines = (UserAdditionalInline, )
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
