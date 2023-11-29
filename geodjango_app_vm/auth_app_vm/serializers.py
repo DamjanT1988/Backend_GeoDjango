2601a8f428f1d57430dac428a9ee7dc17760961c
DamjanT1988 <Damjan_t@hotmail.com>
2023-11-29 12:06:13 +0100

    291123-1200-innan JWT

--- geodjango_app_vm/auth_app_vm/serializers.py
+++ geodjango_app_vm/auth_app_vm/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-from .models import User_additional  
+from .models import User_additional  # Ensure this import is correct based on your project structure
 
 class UserAdditionalSerializer(serializers.ModelSerializer):
     class Meta:
@@ -12,14 +12,16 @@ class UserCreationSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password', 'user_additional']
+        fields = ['username', 'email', 'password', 'first_name', 'last_name', 'user_additional']
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
         user_additional_data = validated_data.pop('user_additional', None)
         user = User.objects.create_user(
             username=validated_data['username'],
-            email=validated_data['email']
+            email=validated_data['email'],
+            first_name=validated_data.get('first_name', ''),  # Get first_name
+            last_name=validated_data.get('last_name', '')    # Get last_name
         )
         user.set_password(validated_data['password'])
         user.save()
@@ -28,20 +30,3 @@ class UserCreationSerializer(serializers.ModelSerializer):
             User_additional.objects.create(user=user, **user_additional_data)
 
         return user
-
-"""
-class UserCreationSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password']
-        extra_kwargs = {'password': {'write_only': True}}
-
-    def create(self, validated_data):
-        user = User.objects.create_user(
-            username=validated_data['username'],
-            email=validated_data['email']
-        )
-        user.set_password(validated_data['password'])
-        user.save()
-        return user
-"""
