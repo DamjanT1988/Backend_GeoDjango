a93c18b5e11e79d9bf5a5384dd7f9d1d38d94ae3
DamjanT1988 <Damjan_t@hotmail.com>
2023-11-29 15:45:03 +0100

    291123-1530-klar audit text i admin

--- geodjango_app_vm/auth_app_vm/models.py
+++ geodjango_app_vm/auth_app_vm/models.py
@@ -3,14 +3,22 @@ from django.contrib.auth.models import User
 
 class User_additional(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    organization_name = models.CharField(max_length=255, null=True, blank=True)
-    role = models.CharField(max_length=255, null=True, blank=True)
-    job_title = models.CharField(max_length=255, null=True, blank=True)
-    department = models.CharField(max_length=255, null=True, blank=True)
-    phone_number = models.CharField(max_length=100, null=True, blank=True)
+    organization_name = models.CharField("Organisation:", max_length=255, null=True, blank=True)
+    role = models.CharField("Roll:", max_length=255, null=True, blank=True)
+    job_title = models.CharField("Jobbtitel:", max_length=255, null=True, blank=True)
+    department = models.CharField("Avdelning:", max_length=255, null=True, blank=True)
+    phone_number = models.CharField("Telefonnummer:", max_length=100, null=True, blank=True)
+
+    class Meta:
+            verbose_name = "Ytterligare användarinformation"
+            verbose_name_plural = "Ytterligare användarinformation"
 
 class Audit_log(models.Model):
-    action = models.CharField(max_length=255, null=True, blank=True)
-    details = models.TextField(null=True, blank=True)
-    timestamp = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    action = models.CharField("Aktion:", max_length=255, null=True, blank=True)
+    details = models.TextField("Information:", null=True, blank=True)
+    timestamp = models.DateTimeField("Tidslag:", auto_now_add=True, null=True, blank=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    class Meta:
+            verbose_name = "Revisionslogg"
+            verbose_name_plural = "Revisionslogg"
