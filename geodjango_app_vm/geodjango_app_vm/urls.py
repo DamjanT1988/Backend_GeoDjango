0cebdb4f7e938b6f334fae00c7e7781913a2faec
DamjanT1988 <Damjan_t@hotmail.com>
2024-03-05 12:32:20 +0100

    240305-1230

--- geodjango_app_vm/geodjango_app_vm/urls.py
+++ geodjango_app_vm/geodjango_app_vm/urls.py
@@ -38,9 +38,6 @@ urlpatterns = [
     #fetch projects
     path('api/projects/', ProjectListCreateView.as_view(), name='project-list'),
     path('api/projects/<int:pk>/', ProjectDetailView.as_view(), name='project-detail'),
-    #path('api/projects/<int:project_id>/add_polygon/', views.add_polygon_data, name='add_polygon_data'),
-    #path('api/projects/<int:project_id>/add_line/', views.add_line_data, name='add_line_data'),
-    #path('api/projects/<int:project_id>/add_point/', views.add_point_data, name='add_point_data'),
     #create files
     path('api/projects/get/<str:userID>/<str:projectID>/file', views.get_geojson, name='get_geojson'),
     path('api/projects/post/<str:userID>/<str:projectID>/file', views.save_geojson, name='save_geojson'),
